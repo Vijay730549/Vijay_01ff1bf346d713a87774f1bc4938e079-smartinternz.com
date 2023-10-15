982db46557d3b9aae819814e13c838a7fe2cab90
Vijay730549 <147975631+Vijay730549@users.noreply.github.com>
2023-10-15 15:00:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-class Bank_Account:
-    def __init__(self):
-        self.balance=0
-        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
- 
-    def deposit(self):
-        amount=float(input("Enter amount to be Deposited: "))
-        self.balance += amount
-        print("\n Amount Deposited:",amount)
- 
-    def withdraw(self):
-        amount = float(input("Enter amount to be Withdrawn: "))
-        if self.balance>=amount:
-            self.balance-=amount
-            print("\n You Withdrew:", amount)
-        else:
-            print("\n Insufficient balance  ")
- 
-    def display(self):
-        print("\n Net Available Balance=",self.balance)
- 
-# Driver code
-  
-# creating an object of class
-s = Bank_Account()
-  
-# Calling functions with that class object
-s.deposit()
-s.withdraw()
-s.display()
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
